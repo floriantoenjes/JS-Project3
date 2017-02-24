79b6a2e473fad4eebfd484fb86307b4fcb159390
Florian Tönjes <skatepainter@yahoo.de>
2017-02-24 15:18:25 +0100

    Activities fully implemented now

--- js/main.js
+++ js/main.js
@@ -85,24 +85,7 @@ $("#payment").change(function () {
     }
 });
 
-
-
-
-//const time1 = $labelJSFrameworks.text().match(/\w+\s\d+[ap]m-\d+[ap]m/)[0];
-
-function getPrice($element) {
-    return parseFloat($element.text().match(/\$(\d+)/)[1]);
-}
-
-function getDatetime($element) {
-    const results = $element.text().match(/\w+\s\d+[ap]m-\d+[ap]m/);
-    if (results !== null) {
-        return results[0];
-    }
-}
-
-
-// Bind a function to change event of activity checkboxes
+// Bind a function to the change event of the activity checkboxes
 const $activities = $(".activities");
 $(".activities input").change(function (evt) {
     let sum = 0;
@@ -112,29 +95,24 @@ $(".activities input").change(function (evt) {
     // Get the sum of this activity
     if (this.checked) {
         sum += getPrice($label);
-    } else {
-        datetime = "free";
     }
 
     // Add up the cost of checked siblings
     $label.siblings("label").toArray().forEach(sibling => {
-
         const input = sibling.firstChild;
-
 
         if (input.checked) {
             const price = getPrice($(sibling))
             sum += price;
         }
 
-
         // Check if the time is already occupied
         let datetimeOfSibling = getDatetime($(sibling));
-        if (datetime === datetimeOfSibling) {
+        if (this.checked && datetime === datetimeOfSibling) {
             console.log(sibling.textContent);
             $(input).attr("disabled", true);
             sibling.style = "color: grey";
-        } else {
+        } else if (!this.checked && datetime === datetimeOfSibling) {
             $(input).attr("disabled", false);
             sibling.style = "color: black";
         }
@@ -147,3 +125,17 @@ $(".activities input").change(function (evt) {
     }
 
 });
+
+
+// Extract the dollar price
+function getPrice($element) {
+    return parseFloat($element.text().match(/\$(\d+)/)[1]);
+}
+
+// Extract the datetime
+function getDatetime($element) {
+    const results = $element.text().match(/\w+\s\d{1,2}[ap]m-\d{1,2}[ap]m/);
+    if (results !== null) {
+        return results[0];
+    }
+}
